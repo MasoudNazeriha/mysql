4aa6021d00e9fb8184ab6d1ea5bf2bb1024d82bb
MasoudNazeriha <153667625+MasoudNazeriha@users.noreply.github.com>
2024-04-16 11:42:28 +0330

    Update second.py

--- second.py
+++ second.py
@@ -5,7 +5,8 @@ import pandas as pd
 # Connect to MySQL database
 try:
     connection = mysql.connector.connect(
-        host="127.0.0.1:3306",
+        host="127.0.0.1",
+        port="3306"
         user="root",  # Replace with your MySQL username
         password="",  # Replace with your MySQL password
         database="railseir"  # Replace with your database name
